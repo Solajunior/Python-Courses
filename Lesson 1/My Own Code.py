1c25bc6d54c6200bb518e9979d75b510fca6ddac
Solajunior <solasamuelinc@gmail.com>
2026-05-15 18:54:19 -0400

    Data Types In Python

---
+++ Lesson 1/My Own Code.py
@@ -0,0 +1,4 @@
+print("This is a print statement.")
+print("It is used for printing strings or words.")
+print("I'm using it right now!")
+print("Well, that's all for now, bye!")
---
+++ Typecasting.py
@@ -0,0 +1,22 @@
+name="Sola"
+age=8
+is_student=True
+weight=20.5
+
+print("name: ", name)
+print("data type of name is", type (name))
+print("age: ", age)
+print("data type of age is", type (age))
+print("is_student: ", is_student)
+print("data type of is_student is", type (is_student))
+print("weight: ", weight)
+print("data type of weight is", type (weight))
+
+
+print("\nAfter Type Casting....")
+age=str(age)
+print(age)
+print("data type of age is", type (age))
+weight=int(weight)
+print(weight)
+print("data type of weight is", type (weight))
