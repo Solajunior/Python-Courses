f77d4a9f3759529e246d57cacbc5d970f1a44da6
Solajunior <solasamuelinc@gmail.com>
2026-05-19 18:44:05 -0400

    Python Operators Part 1

---
+++ Lesson 3/Average.py
@@ -0,0 +1,10 @@
+tree=12
+tree2=19
+tree3=15
+tree4=23
+tree5=27
+
+sum=tree+tree2+tree3+tree4+tree5
+print("The sum of all the five trees is ", sum)
+Average=sum/5
+print("The average of all the trees are", Average)
