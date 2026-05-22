5bf2efd326c5a5cfe5fd32804fdcdd44c9cab255
Solajunior <solasamuelinc@gmail.com>
2026-05-22 18:42:45 -0400

    Conditional Statements

---
+++ Lesson 4/Conditional Statements.py
@@ -0,0 +1,7 @@
+num=13
+if num > 5:
+    print(num, "is bigger than 5")
+
+num2=-9
+if num > 5:
+    print(num2, "is bigger than 5")
---
+++ Lesson 4/Profit Loss.py
@@ -0,0 +1,7 @@
+actual_cost=float(input("please enter the actual product price: "))
+sale_amount=float(input("please enter the sale amount price: "))
+if (sale_amount > actual_cost):
+    amount=sale_amount-actual_cost
+    print("Total Profit = {0}".format(amount))
+else:
+    print("No Profit!!!")
