73ca21073398f3da6c630dc8c21b168fd26cd4f6
Solajunior <solasamuelinc@gmail.com>
2026-05-26 18:42:42 -0400

    Python Operators Part 2

---
+++ Lesson 5/Logical Operator.py
@@ -0,0 +1,22 @@
+a=25
+b=50
+c=50
+
+print(not (a==b))
+print(not (b==c))
+
+a="laptop"
+b="phone"
+if not (a == b):
+    print(a, 'and', b, 'are different')
+
+    a=9
+    b=22
+
+    if not ((a == 1) == (b == 22)):
+        print('Hello')
+
+        a = int(input("Enter A Number: "))
+
+        if not (a % 2 == 0):
+            print(a, "is an odd number.")
