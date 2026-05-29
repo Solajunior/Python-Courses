39417aa676223cb505433b58f32ae9c0abba6de0
Solajunior <solasamuelinc@gmail.com>
2026-05-29 19:05:41 -0400

    After Class Projects

---
+++ After Class Projects/Class Project 1.py
@@ -0,0 +1,9 @@
+age = int(input("Enter Your Age: "))
+if (age >= 10):
+   if (age <= 20):
+      print("You are above 9 years old and less than 21 years old.")
+else:
+   if (age >= 21):
+      print("You are above 20 years old.")
+   else:
+      print("You are younger than 10.") 
