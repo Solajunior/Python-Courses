67f339f2030fa78f388417d0294f77ca9da75d93
Solajunior <solasamuelinc@gmail.com>
2026-05-29 19:46:30 -0400

    After Class Projects

---
+++ After Class Projects/Class Projects 3.py
@@ -0,0 +1,11 @@
+birthday1="September, 12, 2011"
+birthday2="April, 5, 2009"
+birthday3="November, 28, 2014"
+birthday4="May, 3, 2017"
+birthday5="July, 19, 2012"
+print("Birthday 1: ", birthday1)
+print("Birthday 2: ", birthday2)
+print("Birthday 3: ", birthday3)
+print("Birthday 4: ", birthday4)
+print("Birthday 5: ", birthday5)
+print("Those are all the birthdays!")
