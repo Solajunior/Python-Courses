aa59c7e67edd7de659fd031f4dcfbb3ec74ec15f
Solajunior <solasamuelinc@gmail.com>
2026-07-07 18:25:10 -0400

    Random and Math Module

---
+++ Lesson 15/Number Game.py
@@ -0,0 +1,13 @@
+import random
+Playing = True
+number = str(random.randint(1, 20))
+print("I will generate a number from 1 to 20, and you have to guess the number one digit at a time.")
+print("The game ends when you get 1 hero!")
+while Playing:
+    guess = input("Give me your best guess! \n")
+    if number == guess:
+        print("You win!")
+        print("The number was", number)
+        break
+    else:
+        print("Your guess isn't quite right, try again. \n")
